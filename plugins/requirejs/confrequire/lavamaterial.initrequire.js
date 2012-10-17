db61feae4d5e6de8f9d0d13738fa228425146b81
Thomas Bonk <thomas.bonk@gateprotect.com>
2012-10-17 17:42:22 +0200

    Clean start with EaselJS

--- plugins/requirejs/confrequire/lavamaterial.initrequire.js
+++
@@ -1,7 +0,0 @@
-define(['module', 'plugins/lavamaterial/tquery.lavamaterial'], function(module){
-	// compute the absoute baseUrl for this file.js
-	var baseUrl	= location.href.replace(/[^/]*$/, '')
-			+ module.uri.replace(/[^/]*$/, '');
-	// set baseUrl for this plugin
-	tQuery.Object3D.prototype.setLavaMaterial.baseUrl	= baseUrl+'../../lavamaterial/';
-});
--- plugins/requirejs/confrequire/planets.initrequire.js
+++
@@ -1,7 +0,0 @@
-define(['module', 'plugins/planets/tquery.createplanet'], function(module){
-	// compute the absoute baseUrl for this file.js
-	var baseUrl	= location.href.replace(/[^/]*$/, '')
-			+ module.uri.replace(/[^/]*$/, '');
-	// set baseUrl for this plugin
-	tQuery.createPlanet.baseUrl	= baseUrl+'../../planets/';
-});
