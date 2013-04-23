8606ecba109cc042bc7c4b5a12cdcd5300c1da06
Thomas Bonk <thomas.bonk@gateprotect.com>
2013-04-23 21:19:33 +0200

    Prepared hexagon grid

--- app/space.ts
+++ app/space.ts
@@ -4,14 +4,33 @@ class Space {
     private static _instance:Space;
     private objects: THREE.Object3D[] = [];
     private scene: THREE.Scene;
+    private grid: THREE.Object3D;
+    private edgeLength: number = 40;
 
     constructor()
     {
+        var a = this.edgeLength;
+        var b = a/Math.sqrt(2);
+        var startX: number = 100;
+        var startY: number = 100;
+        var geometry: THREE.Geometry = new THREE.Geometry();
+        geometry.vertices.push(new THREE.Vector3(startX, 0, 0));
+        geometry.vertices.push(new THREE.Vector3(startX+b, b, 0));
+        geometry.vertices.push(new THREE.Vector3(startX+b+a, b, 0));
+        geometry.vertices.push(new THREE.Vector3(startX+b+a+b, 0, 0));
+//        geometry.vertices.push(new THREE.Vector3(startX+b+a+b+a, 0, 0));
+//        geometry.vertices.push(new THREE.Vector3(startX+b+a+b+a+b, b, 0));
+
+        var material = new THREE.LineBasicMaterial({
+            color: 0x555555
+        });
+        this.grid = new THREE.Line(geometry , material);
     }
 
     public setScene(s: THREE.Scene)
     {
         this.scene = s;
+        this.scene.add(this.grid);
     }
 
     public static getInstance():Space
